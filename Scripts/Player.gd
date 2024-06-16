11d8da758312b56e1daebdcaf3bf6ddff8a37f2c
Thimble <lojith@proton.me>
2024-06-16 12:29:59 +0100

    added a delayed grapple jump

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -9,56 +9,29 @@ const acceleration_speed = speed * 6.0
 const gravity_normal = 500
 const gravity_grappling = 50
 
+@onready var animator = $"AnimatedSprite2D"
+
 @export var jump_speed = -181.25
 @export var terminal_velocity = 225
-
-@onready var animator = $"AnimatedSprite2D"
-@onready var label = $"Label"
-@onready var raycast_left : Raycast2DCollisionEvents = $"RayCast2D_Left"
-@onready var raycast_right : Raycast2DCollisionEvents = $"RayCast2D_Right"
-
-# Get the gravity from the project settings so you can sync with rigid body nodes.
 @export var gravity = 500
+
 var is_grappling : bool = false
 var last_grapple : Direction = Direction.NONE
 var is_paused = false
-
-var is_against_left_wall:
-	get: return raycast_left.is_colliding()
-
-var is_against_right_wall:
-	get: return raycast_right.is_colliding()
 
 func _physics_process(delta):
 	if is_paused:
 		return
 	
-	# jump
-	if Input.is_action_just_pressed("jump"):
-		try_jump()
-	elif Input.is_action_just_pressed("jump") and velocity.y < 0.0:
-		velocity.y *= 0.6
-		
-	# gravity
+	handle_jump()
 	velocity.y = minf(terminal_velocity, velocity.y + gravity * delta)
-		
-	movement(delta)
+	handle_movement(delta)
 	handle_animations()
 	move_and_slide()
 
-func try_jump() -> void:
-	if is_on_floor():
-		label.text = ""
-	elif is_on_wall() and is_grappling:
-		StopGrappling()
-		velocity.x *= 2.5
-	else:
-		return
-	velocity.y = jump_speed
-
 func handle_animations():
 	# play animations
-	if !is_on_floor() and !is_on_wall():
+	if !is_on_floor() and !is_grappling:
 		if velocity.y <= 0.0:
 			animator.play("jumping")
 		if velocity.y > 0.0:
@@ -70,12 +43,10 @@ func handle_animations():
 			animator.play("moving")
 		else:
 			animator.play("idle")
-	elif is_on_wall():
-		if is_grappling:
-			animator.play("grappling")
+	if is_grappling:
+		animator.play("grappling")
 	pass
-
-func movement(delta : float):
+func handle_movement(delta : float):
 	var direction : float = 0.0
 	if Input.is_key_pressed(KEY_A): direction = -1.0
 	if Input.is_key_pressed(KEY_D): direction = 1.0
@@ -87,23 +58,31 @@ func movement(delta : float):
 			animator.scale.x = 1.0
 		else:
 			animator.scale.x = -1.0
+func handle_jump():
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor() or is_grappling:
+			StopGrappling()
+			velocity.y = jump_speed
+			if is_on_floor():
+				emit_jump_particle()
 
 func StartGrappling():
 	is_grappling = true
 	# set velocity.y to 0.0 to stop any jump momentum
 	velocity.y = 0.0
-	SpawnGrapplingParticles()
+	emit_grappling_particles()
 	gravity = gravity_grappling
-	print("Start Grappling")
 func StopGrappling():
 	is_grappling = false
 	gravity = gravity_normal
-	print("Stop Grappling")
 
-func SpawnGrapplingParticles():
+func emit_grappling_particles():
 	while is_grappling:
-		print("Spawn grappling particles!")
-		await get_tree().create_timer(1.0).timeout
+		await get_tree().create_timer(0.33).timeout
+func emit_jump_particle():
+	pass
+func emit_head_bump_particle():
+	pass
 
 func pause():
 	is_paused = true
@@ -113,24 +92,33 @@ func unpause():
 func _on_wall_contact(direction : Direction):
 	if is_on_floor():
 		return
-		
-	StartGrappling()
-	last_grapple = direction
-		
+	
+	# perevent grappling the same wall twice
+	if direction != last_grapple:
+		StartGrappling()
+		last_grapple = direction
 func _on_break_wall_contact(direction : Direction):
 	StopGrappling()
-	
+	if velocity.y > 0:
+		delayed_grapple_jump()
 func _on_floor_contact(direction : Direction):
 	if is_grappling:
 		StopGrappling()
 	last_grapple = Direction.NONE
-	
 func _on_break_floor_contact(direction : Direction):
 	pass
-
 func _on_ceiling_contact(direction : Direction):
+	emit_head_bump_particle()
 	pass
-	
 func _on_break_ceiling_contact(direction : Direction):
 	pass
 
+func delayed_grapple_jump():
+	var timer : float = 0.0
+	var duration : float = 0.125
+	while timer < duration:
+		timer += get_process_delta_time()
+		if Input.is_key_pressed(KEY_SPACE):
+			velocity.y = jump_speed
+			return
+		await get_tree().create_timer(0.0).timeout 
