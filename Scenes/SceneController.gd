3c901a7ea9b37d816aae9f4a01a263b1fccfcfbb
Thimble <lojith@proton.me>
2024-06-18 21:53:21 +0100

    fixed viewport to { 512, 512 }

--- Scenes/SceneController.gd
+++ Scenes/SceneController.gd
@@ -5,6 +5,9 @@ extends Node2D
 @export var actors : Array[Actor] = []
 var enabled = true
 
+func _ready():
+	get_viewport().size = Vector2i(512, 512)
+
 func _physics_process(delta):
 	if enabled:
 		for actor in actors:
